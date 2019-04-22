68e1bb9bb38fab3269fadc3eb49e611c8a51c480
BenoitPhilips <Benoit.Philips@GMail.com>
2019-04-22 13:20:06 +0700

    App Store was integrated including saving user defaults.
    Be careful while testing as this application can include in App purchase. use a sandbox user id.

--- InspoQuotes-BPH/Controllers/QuoteTableViewController.swift
+++ InspoQuotes-BPH/Controllers/QuoteTableViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import StoreKit
 
-class QuoteTableViewController: UITableViewController {
+class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
     
-    let productId = ""
+    let productId = "com.HumbeekWave.InspoQuotesBPH.Premium"
+    var premiumQuotesPurchased : Bool? = nil
     
-    var quotesToShow = [
+    
+    var quotes = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
         "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
         "It does not matter how slowly you go as long as you do not stop. – Confucius",
@@ -30,26 +33,37 @@ class QuoteTableViewController: UITableViewController {
         "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
     ]
 
+    var quotesToShow : [String] = []
+
     //----------------------------------------------------------------------------------------------------------
     //MARK: - Loading Navigationcontroller
     //
     override func viewDidLoad() {
         super.viewDidLoad()
+        SKPaymentQueue.default().add(self)
+         premiumQuotesPurchased = checkIfPremiumIsPurchased()
+        loadQuotes()
      }
 
     //----------------------------------------------------------------------------------------------------------
     //MARK: - Tableview Datasource Methods
     //
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return quotesToShow.count + 1
+        if let purchased = premiumQuotesPurchased {
+            return premiumQuotesPurchased! ? quotesToShow.count : quotesToShow.count + 1
+        } else {
+            return quotesToShow.count + 1
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell" , for: indexPath)
         if indexPath.row < quotesToShow.count {
             cell.textLabel?.text = quotesToShow[indexPath.row]
+            cell.textLabel?.textColor = .black
             cell.textLabel?.numberOfLines = 0
-        } else {
+            cell.accessoryType = .none
+    } else {
             cell.textLabel?.text = "Buy extra quotes"
             cell.textLabel?.textColor = #colorLiteral(red: 0.3647058824, green: 0.568627451, blue: 1, alpha: 1)
             cell.accessoryType = .disclosureIndicator
@@ -70,12 +84,63 @@ class QuoteTableViewController: UITableViewController {
     //----------------------------------------------------------------------------------------------------------
     //MARK: - In-App Purchase Methods
     //
-   func buyPremiumQuotes(){
+    func buyPremiumQuotes(){
+        if SKPaymentQueue.canMakePayments() {
+            let payementRequest = SKMutablePayment()
+            payementRequest.productIdentifier = productId
+            SKPaymentQueue.default().add(payementRequest)
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         
+        var errorDescription : String
+        
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchasing :
+                print("BPH: transaction has state 'Purchasing'")
+            case .purchased, .restored :
+                SKPaymentQueue.default().finishTransaction(transaction)
+                premiumQuotesPurchased = true
+                UserDefaults.standard.set(true, forKey: productId)
+                loadQuotes()
+            case .failed :
+                if let error = transaction.error {
+                    errorDescription = error.localizedDescription
+                } else {
+                    errorDescription = "no error description received"
+                }
+                print("BPH: transaction has state .failed with following desciprion : \(errorDescription)")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .deferred :
+                print("BPH: transaction has state 'deferred'")
+            default:
+                print("BPH: unknown app store transaction state \(transaction.transactionState) encountered...")
+            }
+        }
+    }
+    
+    func loadQuotes() {
+        quotesToShow = quotes
+        if let purchased = premiumQuotesPurchased {
+            if purchased {
+                quotesToShow.append(contentsOf: premiumQuotes)
+                navigationItem.setRightBarButton(nil, animated: true)
+            }
+        }
+    }
+    
+    func checkIfPremiumIsPurchased() -> Bool? {
+        if UserDefaults.standard.object(forKey: productId) == nil {
+            return nil
+      } else {
+            return(UserDefaults.standard.bool(forKey: productId))
+        }
     }
     
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
-        
+      SKPaymentQueue.default().restoreCompletedTransactions()
     }
 
 }
